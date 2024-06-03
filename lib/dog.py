29b2317758b4542f6eb13c142ebfb1c7986fecb9
lidyaghilay <lidya.ghilay@student.moringaschool.com>
2024-06-03 23:41:04 +0300

    Done

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,14 @@
-#!/usr/bin/env python3
-
 class Dog:
     # Class body goes here
 
-    #Instance method definition
-    pass
+    # Instance method definition
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):  # <--- Move the sit method inside the class definition
+        print("The dog is sitting.")
+
+if __name__ == "__main__":
+    fido = Dog()
+    fido.bark()  
+    fido.sit()
